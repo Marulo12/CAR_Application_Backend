8cf68b17d2a0652b1bfd208f64c7e939b1e3dec0
mboscatto <mariolucbos@gmail.com>
2023-07-12 12:07:09 -0300

    add validator on get model by id brand

---
+++ Application/Feactures/Models/Queries/GetModelsByIdBrand/GetModelsByIdBrandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+
+namespace Application.Feactures.Models.Queries.GetModelsByIdBrand
+{
+    public class GetModelsByIdBrandValidator: AbstractValidator<GetModelsByIdBrand>
+    {
+        public GetModelsByIdBrandValidator() {
+            RuleFor(s => s.IdBrand)
+                .NotEmpty().WithMessage("El id de la marca es requerido")
+                .NotNull().WithMessage("El id de la marca es requerido");
+        }
+    }
+}
